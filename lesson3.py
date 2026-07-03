583e12ddca1472190dc12a832731701cc0f38098
SadaqatQadri <Sadaqat3758@gmail.com>
2026-07-03 21:38:48 +0500

    Lesson 3 - functions and env setup

---
+++ lesson3.py
@@ -0,0 +1,5 @@
+def greet(name):
+    print("Good evening, " + name + "! I am JARVIS, your personal assistant. How may I assist you today?")
+          
+greet("Lord Sadaqat the Great")
+
--- weather.py
+++ weather.py
@@ -1,17 +1,30 @@
 import requests
+from dotenv import load_dotenv
+import os
 
-api_key = "c0a55dcc88027253b4f76ad28f234425"
-city = input("Which city, sir? ")
+load_dotenv()
+api_key = os.getenv("WEATHER_API_KEY")
+def get_weather(city):
+  
+    url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + api_key + "&units=metric"
 
-url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + api_key + "&units=metric"
+    response = requests.get(url)
+    data = response.json()
 
-response = requests.get(url)
-data = response.json()
+    if data.get ("cod") != 200:
+        print("Sorry sir, I couldnt find the weather for " + str(data.get("message", "Unknown error")))
+    else:    
+        temp = data["main"]["temp"]
+        condition = data["weather"][0]["description"]
+        humidity = data ["main"]["humidity"]
+    
+        print("Sir, the weather in " + city + " is " + condition)
+        print("Temperature: " + str(temp) + "°C")
+        print("Humidity: " + str(humidity) + "%")
 
-temp = data["main"]["temp"]
-condition = data["weather"][0]["description"]
-humidity = data ["main"]["humidity"]
-
-print("Sir, the weather in " + city + " is " + condition)
-print("Temperature: " + str(temp) + "°C")
-print("Humidity: " + str(humidity) + "%")
+city = input("Which city should I search for, sir?")
+get_weather(city)
+another = input("Would you like to check the weather for another city? (yes/no): ").strip().lower()
+if another == "yes":
+    city2 = input("Which city, sir?")
+    get_weather(city2)
