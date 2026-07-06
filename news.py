6bcecda9c4d4ef6bacd7f6b60be27b191a30a8ae
SadaqatQadri <Sadaqat3758@gmail.com>
2026-07-06 21:10:30 +0500

    News briefing with country and topic filter

--- news.py
+++ news.py
@@ -4,17 +4,31 @@ import os
 
 load_dotenv()
 
-news_api_key = os.getenv("NEWS_API_KEY")   
+news_api_key = os.getenv("NEWS_API_KEY")
 
-def get_news():
-    url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=" + news_api_key
-    response = requests.get(url)
-    data = response.json()  
+def get_news(country="us", topic=None):
+    if topic:
+        url = "https://newsapi.org/v2/top-headlines?country=" + country + "&category=" + topic + "&apiKey=" + news_api_key
+    else: 
+        url = "https://newsapi.org/v2/top-headlines?country=" + country + "&apikey=" + news_api_key
+
+    response = requests.get(url)   
+    data = response.json()
 
     articles = data["articles"]
 
-    print("Here are the top headlines for today, sir:")
+    if not articles:
+        print("No news found for that particular search, sir.")
+        return
+
+    print ("Here are the top headlines, sir:")
     for i, article in enumerate(articles[:5]):
         print(str(i+1) + ". " + article["title"])
 
-get_news()
+country = input("Which country would you like me to get the news from, sir? (e.g. us, gb, pk): ")
+topic = input("Any specific topic? (technology/business/entertainment/sports/leave blank): ")
+
+if topic == "":
+    get_news(country)
+else:
+    get_news(country, topic)
