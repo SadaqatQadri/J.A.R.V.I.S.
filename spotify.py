4b2743ae60145b2ab8f8969272977ca97b0039cf
SadaqatQadri <Sadaqat3758@gmail.com>
2026-07-07 15:21:46 +0500

    Spotify controls, play, pause, resume, skip, volume, current track

--- spotify.py
+++ spotify.py
@@ -28,5 +28,55 @@ def play_song(song_name):
     sp.start_playback(uris=[track_uri])
     print("Now playing: " + track_name + " by " + artist_name)
 
-song = input("What would you like to listen to, sir? ")
-play_song(song)
+def pause():
+    sp.pause_playback()
+    print("Paused, sir.")
+
+def resume():
+    sp.start_playback()
+    print("Resumed, sir.")
+
+def skip():
+    sp.next_track()
+    print("Skipped to the next track, sir.")
+
+def previous():
+    sp.previous_track()
+    print("Went back to the previous track, sir.")
+
+def volume(level):
+    sp.volume(level)
+    print("Volume set to " + str(level) + ", sir.")
+
+def current_track():
+    track = sp.current_user_playing_track()
+    if track:
+        name = track["item"]["name"]
+        artist = track["item"]["artists"][0]["name"]
+        print("Currently playing: " + name + " by " + artist)
+    else:
+        print("Nothing is currently playing, sir.")
+
+while True:
+        command = input ("Command, sir? (play/pause/resume/skip/back/volume/current/quit): ")
+
+        if command == "play":
+            song = input("Which song, sir? ")
+            play_song(song)
+        elif command == "pause":
+            pause()
+        elif command == "resume":
+            resume()
+        elif command == "skip":
+            skip()
+        elif command == "back":
+            previous()
+        elif command == "volume":
+            level = int(input("Volume level (0-100):"))
+            volume(level)
+        elif command == "current":
+            current_track()
+        elif command == "quit":
+            print("Goodbye, sir.")
+            break
+         
